0309fc1841eb5e8c5b0ae1c4b823c7332aecb707
Dongly <1763394936@qq.com>
2018-04-16 10:14:15 +0800

    启服务

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
 var gulp = require("gulp");
+var server = require("gulp-webserver");//启服务
+var minCss = require("gulp-clean-css");//压缩css
+var imgmin = require("gulp-imagemin");//压缩img
+var htmlmin = require("gulp-htmlmin");//压缩html
+var jsmin = require("gulp-uglify");//压缩js
+
 
 gulp.task("server",function(){
-	
+	gulp.src("./src")
+		.pipe(server({
+			port:8080,
+			open:true,
+			livereload: true,
+            host: 'localhost'
+		}))
 })
+
+//gulp.task("mincss",function(){
+//	
+//})
+//gulp.task("htmlMin",function(){
+//	
+//})
+//gulp.task("minJs",function(){
+//	
+//})
+//gulp.task('default', function(op) {
+//  sequence(['mincss', 'htmlMin', 'minJs'], 'server',op)
+//});
